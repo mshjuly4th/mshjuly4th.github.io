fe3912186c0f75e8590f2bdfb140e0cbd472ebb1
David Shustin <david.shustin@gmail.com>
2017-03-15 00:12:34 -0400

    Change logo

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,12 @@ const shadowProp = '0px 1px 20px 0px black';
 var applyShadow = function(currScroll) {
 	if (shadow == undefined) shadow = currScroll == 0;
 	if (shadow && currScroll == 0) {
-		//document.getElementById("navbar").className = document.getElementById("navbar").className.replace(/(?:^|\s)shadowed(?!\S)/g , ''); //Remove shadow with regex
-		$("#navbar").css("box-shadow", "none");
+		document.getElementById("navbar").className = document.getElementById("navbar").className.replace(/(?:^|\s)shadowed(?!\S)/g , ''); //Remove shadow with regex
+		//$("#navbar").css("box-shadow", "none");
 		shadow = false;
 	} else if (!shadow && currScroll != 0) {
-		//document.getElementById("navbar").className += " shadowed"; //Add shadow
-		$("#navbar").css("box-shadow", shadowProp);
+		document.getElementById("navbar").className += " shadowed"; //Add shadow
+		//$("#navbar").css("box-shadow", shadowProp);
 		shadow = true;
 	}
 };
