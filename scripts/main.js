8fb5009853a95913441b78ca665a92d8f3b972b4
David Shustin <david.shustin@gmail.com>
2017-03-14 22:23:13 -0400

    Add shadow transitions

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 var shadow;
+const shadowProp = '0px 1px 20px 0px black';
 var applyShadow = function(currScroll) {
 	if (shadow == undefined) shadow = currScroll == 0;
-	else if (shadow && currScroll == 0) {
-		document.getElementById("navbar").className = document.getElementById("navbar").className.replace(/(?:^|\s)shadowed(?!\S)/g , ''); //Remove shadow with regex
+	if (shadow && currScroll == 0) {
+		//document.getElementById("navbar").className = document.getElementById("navbar").className.replace(/(?:^|\s)shadowed(?!\S)/g , ''); //Remove shadow with regex
+		$("#navbar").css("box-shadow", "none");
 		shadow = false;
 	} else if (!shadow && currScroll != 0) {
-		document.getElementById("navbar").className += " shadowed"; //Add shadow
+		//document.getElementById("navbar").className += " shadowed"; //Add shadow
+		$("#navbar").css("box-shadow", shadowProp);
 		shadow = true;
 	}
 };
