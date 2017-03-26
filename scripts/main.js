e682fd36ba0d9abfa2a27cf3d05f229e5811c1ae
David Shustin <david.shustin@gmail.com>
2017-03-26 15:30:03 -0400

    Implement new slideshow idea Issue: Strange lack of fading

--- scripts/main.js
+++ scripts/main.js
@@ -17,62 +17,63 @@ var applyShadow = function(currScroll) {
 	}
 };
 var slideInterval;
+var userSelectedSlide = null;
 var changeSlide = function() {
 	currSlide = (currSlide + 1) % slides.length;
-	document.getElementById("swap0").src = slides[currSlide];
-	$("#swap0").addClass("fade");
-	$(".slideselected").removeClass("slideselected");
-	$(".slidepick[slide='" + currSlide + "']").addClass("slideselected");
-	$("#swap0").on("animationend", function() {
-		document.getElementById("swap1").src = slides[currSlide];
-		$("#swap0").css("opacity", 0);
-		$("#swap0").removeClass("fade");
-	});
+}
+var transSlide = function() {
+	$("#slide img[slide=\"" + ((currSlide - 1) % slides.length) + "\"]").css("opacity", 0);
+	$("#slide img[slide=\"" + currSlide + "\"]").css("opacity", 1).removeClass("fade").delay(100);
+	if (userSelectedSlide == currSlide || userSelectedSlide == (currSlide + 1) % slides.length) userSelectedSlide = null;
+	else if (userSelectedSlide != null) {
+		//Just a version of the code above.
+		endSlideshow();
+		$("#slide img[slide=\"" + currSlide + "\"]").css("z-index", -1);
+		currSlide = userSelectedSlide;
+		userSelectedSlide = null;
+		$("#slide img[slide=\"" + currSlide + "\"]")
+			.css("z-index", 0)
+			.addClass("fade")
+			.delay(100)
+			.on("animationend", transSlide);
+		startSlideshow();
+	}
 }
 var startSlideshow = function() {
-	currSlide = (currSlide + 1) % slides.length;
-	document.getElementById("swap0").src = slides[currSlide];
-	$("#swap0").addClass("fade");
-	$(".slideselected").removeClass("slideselected");
-	$(".slidepick[slide='" + currSlide + "']").addClass("slideselected");
-	$("#swap0").on("animationend", function() {
-		document.getElementById("swap1").src = slides[currSlide];
-		$("#swap0").css("opacity", 0);
-		$("#swap0").removeClass("fade");
-	});
 	slideInterval = setInterval(function() {
-		currSlide = (currSlide + 1) % slides.length;
-		document.getElementById("swap0").src = slides[currSlide];
-		$("#swap0").addClass("fade");
-		$(".slideselected").removeClass("slideselected");
-		$(".slidepick[slide='" + currSlide + "']").addClass("slideselected");
-		$("#swap0").on("animationend", function() {
-			document.getElementById("swap1").src = slides[currSlide];
-			$("#swap0").css("opacity", 0);
-			$("#swap0").removeClass("fade");
-		});
+		$("#slide img[slide=\"" + currSlide + "\"]").css("z-index", -1);
+		changeSlide();
+		$("#slide img[slide=\"" + currSlide + "\"]")
+			.css("z-index", 0)
+			.addClass("fade")
+			.on("animationend", transSlide);
 	}, slideTime);
 }
 var endSlideshow = function() {
 	clearInterval(slideInterval);
 }
 $('document').ready(function() {
-	document.getElementById("swap1").src = slides[currSlide];
 	applyShadow(window.scrollY);
 	window.addEventListener('scroll', function(e) {
 		applyShadow(window.scrollY);
 	});
 	for (let i = 0; i < slides.length; i++) {
+		$("#slide").append("<img slide=" + i + " src=\"" + slides[i] + "\" />");
 		$("#pickcontainer").append("<div class=\"slidepick\" slide=" + i + "></div>")
 	}
+	$("#slide img:not(#slide img[slide=\"0\"])").css("z-index", -1).css("opacity", 0);
+	$("#slide img[slide=\"0\"]").css("z-index", 0).css("opacity", 1);
 	startSlideshow();
 	$(".slidepick").click(function() {
-		currSlide = parseInt($(this).attr("slide"));
-		if ($("#swap0").hasClass("fade")) {
-			$("#swap0").removeClass("fade");
-		}
-		endSlideshow();
-		startSlideshow();
-	});
+		userSelectedSlide = parseInt($(this).attr("slide"));
+	})
+	// $(".slidepick").click(function() {
+	// 	currSlide = parseInt($(this).attr("slide"));
+	// 	if ($("#swap0").hasClass("fade")) {
+	// 		$("#swap0").removeClass("fade");
+	// 	}
+	// 	endSlideshow();
+	// 	startSlideshow();
+	// });
 });
 
