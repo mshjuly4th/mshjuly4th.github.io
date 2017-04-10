1b805fa0b0c46e185fd0b85688aec883c7495f72
David Shustin <david.shustin@gmail.com>
2017-04-09 22:49:00 -0500

    Begin Firebase functionality

--- scripts/sponsors.js
+++ scripts/sponsors.js
@@ -7,5 +7,15 @@ var addSponsor = function(label, link, img) {
 	");
 }
 $(document).ready(function() {
-	addSponsor("M&amp;SH July 4th - Dynamic", "http://test.mshjuly4th.com", "images/TRGlogo.png");
+	// addSponsor("MSH July 4th - Dynamic", "http://test.mshjuly4th.com", "images/TRGlogo.png");
+	var ref = firebase.database().ref("sponsors");
+	ref.once("value").then(function(ds) {
+		var data = ds.val();
+		console.log(data);
+		for (var level in data) {
+			for (var s in data[level])
+			var sponsor = data[level][s];
+			addSponsor(sponsor.label, sponsor.link, "images/TRGlogo.png");
+		}
+	});
 });
