7940be7dd77c63d0b5441ad44112a7981f2a8654
David Shustin <david.shustin@gmail.com>
2017-04-21 17:32:45 -0400

    Add FAQ

---
+++ scripts/faq.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	var ref = firebase.database().ref("FAQ");
+	ref.once("value").then(function(ds) {
+		var data = ds.val();
+		for (let qa of data) {
+			$("#faq-container").append("<li class=\"question\">" + qa.q + "<ul class=\"answer\"><li>" + qa.a + "</li></ul></li>");
+		}
+	});
+});
