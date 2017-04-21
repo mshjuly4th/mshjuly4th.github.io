a8f35679bce36a9a675286bb80ae616244d62cba
David Shustin <david.shustin@gmail.com>
2017-04-21 17:44:52 -0400

    Smooth FAQ and add links

--- scripts/faq.js
+++ scripts/faq.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 	ref.once("value").then(function(ds) {
 		var data = ds.val();
 		for (let qa of data) {
-			$("#faq-container").append("<li class=\"question\">" + qa.q + "<ul class=\"answer\"><li>" + qa.a + "</li></ul></li>");
+			$("#faq-container").append("<li class=\"question\" style=\"display: none;\">" + qa.q + "<ul class=\"answer\"><li>" + qa.a + "</li></ul></li>");
+			$(".question").fadeIn();
 		}
 	});
 });
