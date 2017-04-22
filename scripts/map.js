2356d7e760d34e22959282faa2643888b82cd6f7
David Shustin <david.shustin@gmail.com>
2017-04-21 22:12:39 -0400

    Fix map
    Global variable shenanigans

--- scripts/map.js
+++ scripts/map.js
@@ -1,13 +1,17 @@
-var initMapReady = false;
+var eventDetails, markers, map;
+var markMapReady = false;
 const mapCenter = { //Millburn High School
 	lat: 40.7204,
 	lng: -74.3157
 };
 var initMap = function() {
-	if (typeof eventDetails == "undefined") {
-		initMapReady = true;
-		return;
-	}
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 15,
+		center: mapCenter
+	});
+	if (markMapReady) markMap();
+}
+var markMap = function() {
 	let locations = [];
 	//Restructure
 	for (let loc in eventDetails.locations) {
@@ -20,15 +24,11 @@ var initMap = function() {
 			}
 		});
 	}
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 4,
-		center: mapCenter
-	});
 	var places = [];
 	for (let i = 0; i < locations.length; i++) {
 		places.push({
 			infowindow: new google.maps.InfoWindow({
-				content: locations[i].subtitle
+				content: "<h1>" + locations[i].title + "</h1><br>" + locations[i].subtitle
 			}),
 			marker: new google.maps.Marker({
 				position: locations[i].position,
@@ -40,12 +40,14 @@ var initMap = function() {
 			places[i].infowindow.open(map, places[i].marker);
 		});
 	}
-
 }
 if (typeof eventDetails == "undefined") {
+	console.log("test");
 	var ref = firebase.database().ref('eventInfo');
 	ref.once("value").then(function(ds) {
-		var eventDetails = ds.val();
-		if (initMapReady) initMap();
+		eventDetails = ds.val();
+		console.log(eventDetails);
+		if (typeof map != 'undefined') markMap();
+		else markMapReady = true; //wait for map
 	});
 }
