6d3cccfa2f65564843ef465abe51d9517ec3b40d
David Shustin <david.shustin@gmail.com>
2017-04-25 23:05:19 -0400

    Add markdown link parsing

--- scripts/faq.js
+++ scripts/faq.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
 	ref.once("value").then(function(ds) {
 		var data = ds.val();
 		for (let qa of data) {
-			$("#faq-container").append("<li class=\"question\" style=\"display: none;\">" + qa.q + "<ul class=\"answer\"><li>" + qa.a + "</li></ul></li>");
+			let question = qa.q.replace(/\[(.+)\]\((.+)\)/g, function (match, $1, $2, offset, original) {return "<a href=\"" + $2 + "\">" + $1 + "</a>";});
+			let answer = qa.a.replace(/\[(.+)\]\((.+)\)/g, function (match, $1, $2, offset, original) {return "<a href=\"" + $2 + "\">" + $1 + "</a>";});
+			$("#faq-container").append("<li class=\"question\" style=\"display: none;\">" + question + "<ul class=\"answer\"><li>" + answer + "</li></ul></li>");
 			$(".question").fadeIn();
 		}
 	});
